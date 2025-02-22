4e1b69005b31521abd5ae26d90bac9908da632b1
WedithaV <wedithav@gmail.com>
2025-02-22 23:44:36 +0530

    Boolean data type

--- MyFirstProject/DataTypes.cs
+++ MyFirstProject/DataTypes.cs
@@ -23,5 +23,37 @@ namespace MyFirstProject
 
         string firstName = ""; //Can be null
         //char dtx = "";  Can not be null
+
+        //***********************************************************************
+
+        void display1()
+        {
+            //Convert strings to numerical data types
+            string age = "23";
+            int a = Convert.ToInt32(age);
+            Console.WriteLine(a);
+
+            string big = "43349384983";
+            long b = Convert.ToInt64(big);
+            Console.WriteLine(b);
+
+            string pi = "3.245454545454";
+            double d = Convert.ToDouble(pi);
+            Console.WriteLine(d);
+
+            string height = "178.34";
+            float e = Convert.ToSingle(height);
+            Console.WriteLine(e);
+
+            string money = "343434343";
+            decimal f = Convert.ToDecimal(money);
+            Console.WriteLine(f);
+        }
+
+        //************************************************************
+
+        //Boolean values
+        bool status = true;
+
     }
 }
