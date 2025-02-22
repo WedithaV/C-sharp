d5c1d4512c80ac96c541e3eaf3cddb01b18431ed
WedithaV <wedithav@gmail.com>
2025-02-22 22:33:47 +0530

    Numerical Data types

---
+++ MyFirstProject/NumericDataTypes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class NumericDataTypes
+    {
+        int age = 23;
+        long big = 3434343L; //If we do not use "L" .It is consider as int
+        double a = 34.53D; //"D" is optional
+        float b = 3.3F; //If we do not use "F" .It is consider as double
+        decimal money = 3434M; //If we do not use "M" .It is consider  as int
+    }
+}
