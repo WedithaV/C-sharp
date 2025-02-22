3bec3706be5874f2b299c1ff70f43394b552b679
WedithaV <wedithav@gmail.com>
2025-02-23 00:29:22 +0530

    Console input and output

---
+++ MyFirstProject/ConsoleIO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class ConsoleIO
+    {
+        public ConsoleIO() 
+        {
+            Console.Write("Enter your name :  ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age :   ");
+            string age = Console.ReadLine(); // Console.ReadLine() return a string value
+
+            Console.WriteLine();
+            Console.WriteLine("Your name is " + name + " and your age is " + age);
+        }
+    }
+}
--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -9,27 +9,7 @@ namespace MyFirstProject
     internal class Program
     {
         static void Main(string[] args)
-        {
-            //Convert strings to numerical data types
-            string age = "23";
-            int a = Convert.ToInt32(age);
-            Console.WriteLine(a);
-
-            string big = "43349384983";
-            long b = Convert.ToInt64(big);
-            Console.WriteLine(b);
-
-            string pi = "3.245454545454";
-            double d = Convert.ToDouble(pi);
-            Console.WriteLine(d);
-
-            string height = "178.34";
-            float e = Convert.ToSingle(height);
-            Console.WriteLine(e);
-
-            string money = "343434343";
-            decimal f = Convert.ToDecimal(money);
-            Console.WriteLine(f);
+        {        
         }
     }
 }
